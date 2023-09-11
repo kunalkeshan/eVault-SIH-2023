1429215c1a2a7777ed77360dced8611713253563
thelordzeus <arnav55125@gmail.com>
2023-09-11 21:02:31 +0530

    minor fix

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -1,40 +1,21 @@
 import { SiBlockchaindotcom } from "react-icons/si";
+import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="bg-gray-100">
       <div className="relative mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8 lg:pt-24">
-        <div className="absolute end-4 top-4 sm:end-6 sm:top-6 lg:end-8 lg:top-8">
-          <a
-            className="inline-block rounded-full bg-blue-600 p-2 text-white shadow transition hover:bg-blue-500 sm:p-3 lg:p-4"
-            href="#MainContent"
-          >
-            <span className="sr-only">Back to top</span>
-
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M14.707 12.707a1 1 0 01-1.414 0L10 9.414l-3.293 3.293a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 010 1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </a>
-        </div>
-
         <div className="lg:flex lg:items-end lg:justify-between">
           <div>
             <div className="flex justify-center items-center space-x-3 text-gray-600 lg:justify-start">
-              <SiBlockchaindotcom className="text-3xl" />
-              <span className="text-gray-600 text-xl">Legal Ledger</span>
+              <Link href="/" className="flex space-x-3">
+                <SiBlockchaindotcom className="text-3xl" />
+                <span className="text-gray-600 text-xl">Legal Ledger</span>
+              </Link>
             </div>
 
             <p className="mx-auto mt-6 max-w-md text-center leading-relaxed text-gray-500 lg:text-left">
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt
-              consequuntur amet culpa cum itaque neque.
+              Secure Legal Records Management with Blockchain Technology -
+              Scalable and Integrated Solution
             </p>
           </div>
 
