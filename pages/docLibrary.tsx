1fc48b8d6c83c6deb965c34ab518d9758fe59814
Om <ommukherjee28@gmail.com>
2023-09-11 22:38:41 +0530

    Added Dashboard

---
+++ pages/docLibrary.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+
+function docLibrary() {
+  return (
+    <div className = "w-screen h-screen flex flex-col justify-around align-center">
+        <h1 className="font-extrabold text-xl md:text-3xl text-center">Welcome to Document Library !</h1>
+        <div className="w-[95%] md:w-[75%] mx-auto border-black border-[1px] h-[60%] rounded-xl overflow-y-auto">
+            <div className="p-4 border-[1px] flex w-full justify-around rounded-t-xl">
+                <div>Field 1</div>
+                <div>Field 2</div>
+                <div>Field 3</div>
+            </div>
+
+            <div className="p-4 border-[1px] flex w-full justify-around">
+                <div>Field 1</div>
+                <div>Field 2</div>
+                <div>Field 3</div>
+            </div>
+
+            <div className="p-4 border-[1px] flex w-full justify-around">
+                <div>Field 1</div>
+                <div>Field 2</div>
+                <div>Field 3</div>
+            </div>
+
+            <div className="p-4 border-[1px] flex w-full justify-around">
+                <div>Field 1</div>
+                <div>Field 2</div>
+                <div>Field 3</div>
+            </div>
+
+            <div className="p-4 border-[1px] flex w-full justify-around">
+                <div>Field 1</div>
+                <div>Field 2</div>
+                <div>Field 3</div>
+            </div>
+
+            <div className="p-4 border-[1px] flex w-full justify-around">
+                <div>Field 1</div>
+                <div>Field 2</div>
+                <div>Field 3</div>
+            </div>
+
+            <div className="p-4 border-[1px] flex w-full justify-around">
+                <div>Field 1</div>
+                <div>Field 2</div>
+                <div>Field 3</div>
+            </div>
+
+            <div className="p-4 border-[1px] flex w-full justify-around">
+                <div>Field 1</div>
+                <div>Field 2</div>
+                <div>Field 3</div>
+            </div>
+
+            <div className="p-4 border-[1px] flex w-full justify-around">
+                <div>Field 1</div>
+                <div>Field 2</div>
+                <div>Field 3</div>
+            </div>
+
+            <div className="p-4 border-[1px] flex w-full justify-around">
+                <div>Field 1</div>
+                <div>Field 2</div>
+                <div>Field 3</div>
+            </div>
+
+            <div className="p-4 border-[1px] flex w-full justify-around">
+                <div>Field 1</div>
+                <div>Field 2</div>
+                <div>Field 3</div>
+            </div>
+
+            <div className="p-4 border-[1px] flex w-full justify-around">
+                <div>Field 1</div>
+                <div>Field 2</div>
+                <div>Field 3</div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default docLibrary
