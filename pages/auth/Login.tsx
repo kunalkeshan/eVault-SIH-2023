cfc93ab245818cbb202f19248a05dfb7960998d9
thelordzeus <arnav55125@gmail.com>
2023-09-11 22:53:24 +0530

    fix: link routes

--- pages/auth/Login.tsx
+++ pages/auth/Login.tsx
@@ -1,40 +1,78 @@
-export default function Login(){
-    return(
-        <div>
-            <div className="w-full max-w-xs m-auto bg-indigo-100 rounded p-5">
-  <header>
-    <img className="w-20 mx-auto mb-5" src="https://img.icons8.com/fluent/344/year-of-tiger.png" alt="Tiger Icon" />
-  </header>
-  <form>
-    <div>
-      <label className="block mb-2 text-indigo-500" htmlFor="username">Username</label>
-      <input
-        className="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
-        type="text"
-        name="username"
-      />
-    </div>
-    <div>
-      <label className="block mb-2 text-indigo-500" htmlFor="password">Password</label>
-      <input
-        className="w-full p-2 mb-6 text-indigo-700 border-b-2 border-indigo-500 outline-none focus:bg-gray-300"
-        type="password"
-        name="password"
-      />
-    </div>
-    <div>
-      <input
-        className="w-full bg-indigo-700 hover:bg-pink-700 text-white font-bold py-2 px-4 mb-6 rounded"
-        type="submit"
-      />
-    </div>
-  </form>
-  <footer>
-    <a className="text-indigo-700 hover:text-pink-700 text-sm float-left" href="#">Forgot Password?</a>
-    <a className="text-indigo-700 hover:text-pink-700 text-sm float-right" href="#">Create Account</a>
-  </footer>
-</div>
+import Link from "next/link";
 
-        </div>
-    );
+export default function Login() {
+  return (
+    <section className="bg-white">
+      <div className="lg:grid lg:min-h-screen lg:grid-cols-12">
+        <aside className="relative block h-16 lg:order-last lg:col-span-5 lg:h-full xl:col-span-6">
+          <img
+            alt="Pattern"
+            src="https://images.unsplash.com/photo-1605106702734-205df224ecce?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+            className="absolute inset-0 h-full w-full object-cover"
+          />
+        </aside>
+
+        <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
+          <div className="max-w-xl lg:max-w-3xl">
+            <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
+              Great to Have you Back with us.
+            </h1>
+
+            <p className="mt-4 leading-relaxed text-gray-500">
+              Secure Legal Records Management with Blockchain Technology -
+              Scalable and Integrated Solution
+            </p>
+
+            <form action="#" className="mt-8 grid grid-cols-6 gap-6">
+              <div className="col-span-6">
+                <label
+                  htmlFor="Email"
+                  className="block text-sm font-medium text-gray-700 "
+                >
+                  Email
+                </label>
+
+                <input
+                  type="email"
+                  id="Email"
+                  name="email"
+                  className="mt-1 w-full rounded-lg border-gray-500 bg-gray-100 text-sm text-gray-700 shadow-sm p-2"
+                />
+              </div>
+
+              <div className="col-span-6 ">
+                <label
+                  htmlFor="Password"
+                  className="block text-sm font-medium text-gray-700"
+                >
+                  Password
+                </label>
+
+                <input
+                  type="password"
+                  id="Password"
+                  name="password"
+                  className="mt-1 w-full rounded-lg border-gray-500 bg-gray-100 text-sm text-gray-700 shadow-sm p-2"
+                />
+              </div>
+
+              <div className="col-span-6 sm:flex sm:items-center sm:gap-4">
+                <button className="inline-block shrink-0 rounded-md border border-blue-600 bg-blue-600 px-12 py-3 text-sm font-medium text-white transition hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500">
+                  Login In
+                </button>
+
+                <p className="mt-4 text-sm text-gray-500 sm:mt-0">
+                  Dont have an account?
+                  <Link href="/auth/SignIn" className="text-gray-700 underline">
+                    Sign In
+                  </Link>
+                  .
+                </p>
+              </div>
+            </form>
+          </div>
+        </main>
+      </div>
+    </section>
+  );
 }
