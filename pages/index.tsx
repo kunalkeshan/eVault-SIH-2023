cbf49f8a0e5ec6563332bc33f1f289a48d30c988
Om <ommukherjee28@gmail.com>
2023-09-12 11:07:00 +0530

    Dashboard Changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,14 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,7 +17,9 @@ export default function Home() {
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      
+      <div>
+        
+      </div>
     </main>
   )
 }
