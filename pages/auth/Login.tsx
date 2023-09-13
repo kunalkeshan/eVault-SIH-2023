ed1dfcecdbe8c0fc9a080221ac829725a873df33
thelordzeus <arnav55125@gmail.com>
2023-09-13 09:05:31 +0530

    minor change

--- pages/auth/Login.tsx
+++ pages/auth/Login.tsx
@@ -52,7 +52,6 @@ export default function Login() {
             className="absolute inset-0 h-full w-full object-cover"
           />
         </aside>
-
         <main className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6">
           <div className="max-w-xl lg:max-w-3xl">
             <h1 className="mt-6 text-2xl font-bold text-gray-900 sm:text-3xl md:text-4xl">
